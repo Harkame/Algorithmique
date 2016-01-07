dc6e1e40fd97cd097984d8a90b8146ad576502ac
Harkame <louisr.daviaud@gmail.com>
2016-01-07 18:51:44 +0100

    Create ListeTriee.java

---
+++ ListeTriee.java
@@ -0,0 +1,22 @@
+package algo;
+
+public class ListeTriee {
+
+	private Liste liste;
+
+	public ListeTriee() {
+		this.liste = new Liste();
+	}
+
+	public Liste getliste() {
+		return this.liste;
+	}
+
+	public void setliste(Liste l) {
+		this.liste = l;
+	}
+
+	public static void main(String[] Args) {
+
+	}
+}
