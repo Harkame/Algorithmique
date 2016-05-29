9089e574e4d1a5dc291bce4063708f9b681a6a12
Louis <louisr.daviaud@gmail.com>
2016-05-29 15:02:02 +0200

    Update TD4.java

--- src/reseau/TD4.java
+++ src/reseau/TD4.java
@@ -8,6 +8,7 @@ import javax.crypto.*;
 public abstract class TD4 {
 	private static final String password = "password";
 	private static final String password_md5 = "f71dbe52628a3f83a77ab494817525c6";
+	//private static final String password_md5 = "toto";
 	private static final String password_sha = "e6fb06210fafc02fd7479ddbed2d042cc3a5155e";
 	private static MessageDigest md5;
 	private static MessageDigest sha;
@@ -21,51 +22,89 @@ public abstract class TD4 {
 			md5 = MessageDigest.getInstance("MD5");
 			sha = MessageDigest.getInstance("SHA");
 
-			bytes_md5 = password_md5.getBytes("UTF-8");
-			bytes_sha = password_sha.getBytes("UTF-8");
+			bytes_md5 = password_md5.getBytes();
+			bytes_sha = password_sha.getBytes();
+			
+			md5_digest = bytes_md5;
 
-			md5_digest = md5.digest(bytes_md5);
-			sha_digest = sha.digest(bytes_sha);
+			//md5_digest = md5.digest(bytes_md5);
+			//sha_digest = sha.digest(bytes_sha);
 		} catch (Exception e) {
 		}
 	}
 
-	private static String[] code_ascii = { "a", "b", "c", "d", "e", "f", "g",
+	private static String[] repertoire = { "a", "b", "c", "d", "e", "f", "g",
 			"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
 			"u", "v", "w", "x", "y", "z" };
 
-	public final static List<String> generer(int p_taille)
-			throws Exception {
-		List<String> resultats = new ArrayList<String>();
+	public final static ArrayList<String> Generer(int p_taille) {
+		ArrayList<String> resultats = new ArrayList<String>();
 		if (p_taille == 0) {
 			resultats.add("");
 			return resultats;
 		}
-		for (String valeur : generer(p_taille - 1)) {
-			for (int i = 0; i < code_ascii.length; i++) {
-				String t = valeur + code_ascii[i];
-				byte[] t_md5 = t.getBytes("UTF-8");
-				byte[] digest = md5.digest(t_md5);
-				for(byte b : digest){
-					System.out.print(b);
-					
-				}
-				System.out.println();
-				if(Arrays.equals(digest, md5_digest)){
-					System.out.println(valeur);
-					return null;
-				}
-				resultats.add(new String(digest));
+		for (String valeur : Generer(p_taille - 1)) {
+			for (int i = 0; i < repertoire.length; i++) {
+				resultats.add(valeur + repertoire[i]);
 			}
 		}
 		return resultats;
 	}
+	
+	public static String MD5(String md5) {
+		   try {
+		        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+		        byte[] array = md.digest(md5.getBytes());
+		        StringBuffer sb = new StringBuffer();
+		        for (int i = 0; i < array.length; ++i) {
+		          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+		       }
+		        System.out.println(sb);
+		        return sb.toString();
+		    } catch (java.security.NoSuchAlgorithmException e) {
+		    }
+		    return null;
+		}
+	
+	public static String SHA(String md5) {
+		   try {
+		        java.security.MessageDigest md = java.security.MessageDigest.getInstance("SHA");
+		        byte[] array = md.digest(md5.getBytes());
+		        StringBuffer sb = new StringBuffer();
+		        for (int i = 0; i < array.length; ++i) {
+		          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+		       }
+		        System.out.println(sb);
+		        return sb.toString();
+		    } catch (java.security.NoSuchAlgorithmException e) {
+		    }
+		    return null;
+		}
+	
 
 	public static void main(String[] args) throws Exception {
-		List l = generer(5);
+		List <String>l = Generer(4);
+		System.out.println();
+		for(int i = 0; i < l.size(); i++){
+			if(password_md5.equals(MD5(l.get(i)))){
+				System.out.println("Mot original : " + l.get(i));
+				break;
+			}
+		}
+		for(int i = 0; i < l.size(); i++){
+			if(password_sha.equals(SHA(l.get(i)))){
+				System.out.println("Mot original : " + l.get(i));
+				break;
+			}
+		}
+		/*
 		for(byte b : md5_digest){
 			System.out.print(b);
 		}
-		System.out.println("");
+		System.out.println("\nyolo");
+		for(byte b: MD5("toto").getBytes()){
+			System.out.print(b);
+		}
+		*/
 	}
 }
